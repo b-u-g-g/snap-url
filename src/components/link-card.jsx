5ff60d40e60a12a667053e0e5af1d0d91693bb7d
b-u-g-g <tsakshi1708@gmail.com>
2025-08-07 21:54:37 +0530

    added vercel.json

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -54,7 +54,7 @@ const LinkCard = ({url = [], fetchUrls}) => {
         <Button
           variant="ghost"
           onClick={() =>
-            navigator.clipboard.writeText(`https://snapurl.in/${url?.short_url}`)
+            navigator.clipboard.writeText(`https://snap-url-one.vercel.app/${url?.short_url}`)
           }
         >
           <Copy />
--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://snapurl.in/${link}`}
+            href={`https://snap-url-one.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://snapurl.in/{link}
+            https://snap-url-one.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://trimrr.in/${link}`)
+                navigator.clipboard.writeText(`https://snap-url-one.vercel.app/${link}`)
               }
             >
               <Copy />
